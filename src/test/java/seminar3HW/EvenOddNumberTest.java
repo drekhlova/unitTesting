5d780ac7c794fc5667888a5f74a06d15db52ffd9
Daria Rekhlova <dolly1993@yandex.ru>
2023-10-17 22:09:16 +0400

    Добавили комментарии с EvenOddNumberTest и NumberInIntervalTest

--- src/test/java/seminar3HW/EvenOddNumberTest.java
+++ src/test/java/seminar3HW/EvenOddNumberTest.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class EvenOddNumberTest extends EvenOddNumber {
+
+    //Проверка, что число четное
     @Test
     @DisplayName("Проверка четного числа")
     public void testEvenNumber() {
         Assertions.assertTrue(evenOddNumber(2));
     }
 
+    //Проверка, что число нечетное
     @Test
     @DisplayName("Проверка нечетного числа")
     public void testOddNumber() {
--- src/test/java/seminar3HW/NumberInIntervalTest.java
+++ src/test/java/seminar3HW/NumberInIntervalTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class NumberInIntervalTest extends NumberInInterval {
+
+    //Проверка, что число находится в интервале от 25 до 100
     @Test
     @DisplayName("Проверка числа в интервале от 25 до 100")
     public void testNumberInInterval() {
