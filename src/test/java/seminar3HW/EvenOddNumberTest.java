a1504980443119ae3c963d49d699f8d61797d3e1
Daria Rekhlova <dolly1993@yandex.ru>
2023-10-17 22:40:31 +0400

    Добавили HW3

--- src/test/java/seminar3HW/EvenOddNumberTest.java
+++ src/test/java/seminar3HW/EvenOddNumberTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class EvenOddNumberTest extends EvenOddNumber {
 
     //Проверка, что число четное
@@ -13,10 +15,20 @@ public class EvenOddNumberTest extends EvenOddNumber {
         Assertions.assertTrue(evenOddNumber(2));
     }
 
-    //Проверка, что число нечетное
+   //Проверка, что число нечетное
     @Test
     @DisplayName("Проверка нечетного числа")
     public void testOddNumber() {
         Assertions.assertFalse(evenOddNumber(3));
     }
+
+    //Проверка числа на четное или нечетное
+    @Test
+    @DisplayName("Проверка числа на четное")
+    void testEvenOddNumber() {
+        int n = 2;
+        assertEquals(evenOddNumber(n), true);
+    }
+
+
 }
